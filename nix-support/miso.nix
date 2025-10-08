8240e8a380a6bead1be5db2f1b6444c9fb040eb4
Philipp Volguine <phil.volguine@gmail.com>
2025-10-08 16:16:46 -0400

    Simplify this app down (remove module, static images)

--- nix-support/miso.nix
+++ nix-support/miso.nix
@@ -3,12 +3,14 @@
 let
   haskellPackages = nixpkgs.haskellPackages;
 
-  src = nixpkgs.fetchFromGitHub {
-    owner = "dmjio";
-    repo = "miso";
-    rev = "e6798ccb6776352e506dfb47bed6f2c1ff77a859";
-    sha256 = "sha256-rXi0JBngEIpVVYd/NnFs6g7fjL2DyYjTGtpsSUY1PH0=";
-  };
+  # src = nixpkgs.fetchFromGitHub {
+  #   owner = "dmjio";
+  #   repo = "miso";
+  #   rev = "e6798ccb6776352e506dfb47bed6f2c1ff77a859";
+  #   sha256 = "sha256-rXi0JBngEIpVVYd/NnFs6g7fjL2DyYjTGtpsSUY1PH0=";
+  # };
+
+  src = ../../miso;
 
   miso = haskellPackages.callCabal2nix "miso" src {};
 in
