f7e6d9bf939eb9da490b902388dcd422d1d09d09
Philipp Volguine <phil.volguine@gmail.com>
2026-02-14 10:30:40 -0500

    Remove aeson dependency from cabal
    - bump nix miso version to match cabal.project

--- nix-support/miso.nix
+++ nix-support/miso.nix
@@ -7,8 +7,8 @@ let
   src = nixpkgs.fetchFromGitHub {
     owner = "dmjio";
     repo = "miso";
-    rev = "381d09ddba11fdbd2e9fe7ed2eb9b81c914b42da";
-    sha256 = "sha256-nYykeIw3bhQALlajMNzv3SQqu3rnAukyecbw6jLbzUg=";
+    rev = "87f11b0e39576d89ca674d513a4d43bb31f1ce69";
+    sha256 = "sha256-+xaq69x43NjOFxRO4YilWCPyRolXdaCLYnDcRHAiIZk=";
   };
 
   miso = haskellPackages.callCabal2nix "miso" src {};
